fa3f90e495ab887542e3412ee8247e5df08f8061
alejandg1 <josealejandrogomezparedes@gmail.com>
2023-05-16 19:58:09 -0500

    talleres en js

---
+++ talleres en js/JOSÉ_ALEJANDRO_GÓMEZ_PAREDES_TLL3.2.js
@@ -0,0 +1,6 @@
+Ln_valor = 10;
+while (Ln_valor != 0) {
+  console.log("hola");
+  Ln_valor--;
+}
+console.log("fin del programa");
---
+++ talleres en js/JOSÉ_ALEJANDRO_GÓMEZ_PAREDES_TLL3.js
@@ -0,0 +1,22 @@
+Ln_valor=4
+Ln_result=Ln_valor%2
+
+
+if(Ln_result==0){
+    console.log("el número es par");
+}
+else{
+    console.log("El número no es par")
+}
+
+console.log("-----------------------------------------------------");
+
+Ln_valor=28.129
+Ln_parte_entera= Math.floor(Ln_valor)
+console.log("parte entera recuperada: ",Ln_parte_entera);
+
+
+
+console.log("-----------------------------------------------------");
+
+console.log("sin funciones propias para obtener decimales");
